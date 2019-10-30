283ee74a0a9ede658484b839717a6aa333aef284
swoboda <michal.swoboda@outlook.com>
2019-10-30 12:56:01 -0400

    finished Challenge 1

--- bamazon.sql
+++ bamazon.sql
@@ -10,7 +10,7 @@ CREATE TABLE products (
   department_name VARCHAR(45) NULL,
   price DECIMAL(10,2) NULL,
   stock_quantity INT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (item_id)
 );
 
 
@@ -18,7 +18,7 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Stale Dog Food", "Geriatric Supplies", 3.50, 1000);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Gold Plated Potato", "Sport", 41, 1500);
+VALUES ("Gold Plated Potato", "Sport Equipment", 41, 1500);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Pickled Raddish", "Cleaning Supplies", 4.99, 2000);
@@ -30,13 +30,13 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Pickled Turtle", "Cleaning Supplies", 800.8, 3000);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Turtle Bong", "Novelty Gifts", 17.50, 20000);
+VALUES ("Turtle Shaped Bong", "Kitchen Utensils", 17.50, 20000);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Corn", "Sport", 0.50, 3200);
+VALUES ("Plate of Candy Corn", "Sport Equipment", 0.50, 3200);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Turtle Scented Candles", "Home Entertainment", 520.10, 90);
+VALUES ("Scented Candles", "Home Entertainment", 520.10, 90);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Dry Frog Scented Candles", "Home Entertainment", 25.90, 1000);
+VALUES ("Dog Scented Candles", "Home Entertainment", 25.90, 1000);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,9 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+let mysql = require("mysql");
+let inquirer = require("inquirer");
+
+var data = [];
+var product = 0;
+var quant = 0;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,44 +16,94 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+
+    console.log("connected as id " + connection.threadId + "\n");
+
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        data = res;
+
+        parseDatabase(data);
+        itemSelect();
+    });
 });
 
-function runSearch() {
+
+function parseDatabase(params) {
+
+    const header_string = "|item_id| product\t\t| category\t\t| price | \n|------\t|-------i\t\t|-------\t\t|-------|"
+    console.log(header_string);
+
+    for (let index = 0; index < params.length; index++) {
+        let prod = params[index];
+        let product_string = `| ${prod.item_id}\t| ${prod.product_name}\t| ${prod.department_name}\t| ${prod.price}\t|`
+        console.log(product_string);
+    }
+    console.log("\n");
+
+}
+
+function itemSelect(params) {
     inquirer
-        .prompt({
-            name: "action",
-            type: "list",
-            message: "What would you like to do?",
-            choices: [
-                "Find songs by artist",
-                "Find all artists who appear more than once",
-                "Find data within a specific range",
-                "Search for a specific song",
-                "exit"
-            ]
-        })
-        .then(function(answer) {
-            switch (answer.action) {
-                case "Find songs by artist":
-                    artistSearch();
-                    break;
-
-                case "Find all artists who appear more than once":
-                    multiSearch();
-                    break;
-
-                case "Find data within a specific range":
-                    rangeSearch();
-                    break;
-
-                case "Search for a specific song":
-                    songSearch();
-                    break;
-
-                case "exit":
-                    connection.end();
-                    break;
+        .prompt([{
+            name: "prodID",
+            type: "input",
+            message: "Which item would you like to purchase?",
+            validate: function(input) {
+                if (!isNaN(parseInt(input)) && parseInt(input) < data.length) {
+                    return true;
+                } else {
+                    return "Please input a number"
+                }
             }
+        }, {
+            name: "units",
+            type: "input",
+            message: "How many units?",
+            validate: function(input) {
+                if (!isNaN(parseInt(input))) {
+                    return true;
+                } else {
+                    return "Please input a number"
+                }
+            }
+
+        }])
+        .then(function(answer) {
+            product = parseInt(answer.prodID);
+            quant = parseInt(answer.units);
+
+            if (quant < data[product].stock_quantity) {
+                var query = connection.query(
+                    "UPDATE products SET ? WHERE ?", [{
+                            stock_quantity: quant
+                        },
+                        {
+                            item_id: product
+                        }
+                    ],
+                    function(err, res) {
+                        if (err) throw err;
+                    }
+                );
+                console.log("Item Ordered. Your total is: $" + data[product].price * quant + "\n");
+            } else {
+                console.log("Sorry, insufficient quantity!\n");
+            }
+            inquirer.prompt([{
+                    name: "orderAgain",
+                    type: "confirm",
+                    message: "Order a new item?"
+                }
+
+            ]).then(function(answer) {
+
+                if (answer.orderAgain) {
+                    itemSelect();
+                } else {
+                    connection.end()
+                    return true;
+                }
+            })
         });
 }
